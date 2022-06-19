8576297b24e9d2df5a8211b7f66ddcfaab68075b
Flávio Arêas <vflavio9@gmail.com>
2022-06-18 21:12:51 -0300

    chore: set up graphql w/ codegen

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { usePageQuery } from '../generated/graphql';
+
+export default function Home() {
+  const [{ data }] = usePageQuery({
+    variables: {
+      slug: 'about',
+    },
+  });
+
+  return <h1>{data?.page.title}</h1>;
+}
