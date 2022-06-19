2f157d4d7eb29749a5c1785247a096b892720088
Flávio Arêas <vflavio9@gmail.com>
2022-06-18 21:28:46 -0300

    chore: set up tailwindcss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
-import { usePageQuery } from '../generated/graphql';
+import { GetStaticProps } from 'next';
+import { PageDocument, usePageQuery } from '../generated/graphql';
+import { client, ssrCache } from '../lib/urql';
 
 export default function Home() {
   const [{ data }] = usePageQuery({
@@ -9,3 +11,17 @@ export default function Home() {
 
   return <h1>{data?.page.title}</h1>;
 }
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  await client
+    .query(PageDocument, {
+      slug: 'about',
+    })
+    .toPromise();
+
+  return {
+    props: {
+      urqlState: ssrCache.extractData(),
+    },
+  };
+};
