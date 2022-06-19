08bb748f192823e10607dc016eb7019c6ddcc7df
Flávio Arêas <vflavio9@gmail.com>
2022-06-19 16:05:50 -0300

    feat: create Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,32 @@
 import { GetStaticProps } from 'next';
+import { Features } from '../components/Features';
+import { Hero } from '../components/Hero';
+import { Pricing } from '../components/Pricing';
 import { PageDocument, usePageQuery } from '../generated/graphql';
 import { client, ssrCache } from '../lib/urql';
 
 export default function Home() {
   const [{ data }] = usePageQuery({
     variables: {
-      slug: 'about',
+      slug: 'home',
     },
   });
 
-  return <h1>{data?.page.title}</h1>;
+  return (
+    <>
+      <title>Home | JAMStack Blog</title>
+
+      <Hero title={data.page.title} subtitle={data.page.subtitle} />
+      <Features />
+      <Pricing />
+    </>
+  );
 }
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
   await client
     .query(PageDocument, {
-      slug: 'about',
+      slug: 'home',
     })
     .toPromise();
 
